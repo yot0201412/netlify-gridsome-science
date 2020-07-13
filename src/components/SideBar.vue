99151c847c045ea26b65f1a94f9dae95ed48935a
yot0201412 <laughing.my.way0201@gmail.com>
2020-07-13 23:55:13 +0900

    add default style

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -2,11 +2,11 @@
   <div class="m-0">
     <div class="m-2 bg-white p-3">
       <p class="m-1 font-semibold items-center"><g-image alt="tag" src="~/tag_icon.png" class="m-0 mr-1 float-left"/>タグ</p>
-      <span v-for="tag in $static.tags.edges" :key="tag.id" class='text-sm inline-block bg-gray-300 hover:bg-indigo-300 rounded-full px-3 py-1 text-gray-700 m-1'>
+      <div v-for="tag in $static.tags.edges" :key="tag.id" class='text-sm inline-block bg-gray-300 hover:bg-indigo-300 rounded-full px-3 py-1 text-gray-700 m-1'>
         <g-link :key="tag.id" :to="tag.node.path">
-          <p> {{ tag.node.title }}</p>
+          <span class="p-0"> {{ tag.node.title }}</span>
         </g-link>
-      </span>
+      </div>
     </div>
     <SideBarPost />
   </div>
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const plugin = require('tailwindcss/plugin')
+
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [
+    plugin(function({ addBase, config }) {
+      addBase({
+        'h1': { fontSize: config('theme.fontSize.2xl') },
+        'h2': { fontSize: config('theme.fontSize.xl') },
+        'h3': { fontSize: config('theme.fontSize.lg') },
+        'p' : { 'line-height': '2.5rem'},
+        'ol': {'list-style-type': 'decimal'}
+      })
+    })
+  ],
+}

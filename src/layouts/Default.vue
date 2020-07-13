c6bd896ce4648630c868a7ff1409858ef12d6c4e
yot0201412 <laughing.my.way0201@gmail.com>
2020-07-14 02:07:15 +0900

    モバイル対応 ちょっとだけ

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="font-mono leading-normal">
     <div class="w-full max-w-screen-xl mx-auto">
-      <header class="flex bg-white border-b border-gray-200 fixed top-0 inset-x-0 z-100 h-16justify-center">
-          <div class="lg:w-3/5 xl:w-3/5 pl-6 pr-6 lg:pr-8 mx-10">
+      <header class="flex bg-white border-b border-gray-200 lg:fixed xl:fixed top-0 inset-x-0 z-100 h-16justify-center">
+          <div class="lg:w-3/5 xl:w-3/5 w-1/2 px-0 lg:pr-8 xl:pr-8 lg:mx-10 xl:mx-10">
             <g-link to="/" ><g-image alt="Logo" src="~/logo-text.png" width="300"/></g-link>
+          <p class="font-semibold self-center lg:text-base xl:text-base text-xs m-1">- 科学の楽しさを語る -</p>
           </div>
-          <p class="font-semibold self-center">- 科学の楽しさを語る -</p>
-          <nav class="lg:flex lg:items-center lg:justify-between xl:w-1/5 px-6">
-            <g-link class="m-5 hover:text-indigo-600" to="/">Home</g-link>
-            <g-link class="m-5 hover:text-indigo-600" to="/about/">ABOUT ME</g-link>
+          <nav class="items-center justify-between xl:w-1/5 px-6 w-1/2 right-0">
+            <g-link class="lg:m-5 xl:m-5 m-1 hover:text-indigo-600 lg:flex xl:flex hidden" to="/">Home</g-link>
+            <g-link class="lg:m-5 xl:m-5 m-1 hover:text-indigo-600" to="/about/">ABOUT ME</g-link>
             <g-link to="https://twitter.com/yoo_medeng" ><g-image alt="twitter" src="~/twitter_icon.png" width="30"/></g-link>
           </nav>
       </header>
@@ -19,7 +19,7 @@
         </div>
       </div>
       <footer>
-        <p class="float-right">Copyright © 2020 Yo</p>
+        <p class="float-right mx-1">Copyright © 2020 Yo</p>
       </footer>
     </div>
   </div>

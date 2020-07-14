d3202fe94740eb2f5295ecb4dab5c3236d574a68
yot0201412 <laughing.my.way0201@gmail.com>
2020-07-15 00:27:59 +0900

    modify intro

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,13 @@
 <template>
   <Layout>
     <div class="m-2 p-3">
-      <p  class="text-center text-sm lg:text-base xl:text-base">
-        好きな科学の面白さ、生活に役立つ情報を発信したくて始めました。<br>
-        科学全般気になったものを取り上げて記事にしていきます。<br>
-        取り上げて欲しい話題がありましたら、Twitterにご連絡ください！
-      </p>
+      <div class="xl:grid lg:grid grid-rows-1 grid-cols-5">
+        <p  class="text-sm lg:text-base xl:text-base col-start-2 col-span-3">
+          好きな科学の面白さ、生活に役立つ情報を発信したくて始めました。
+          科学全般気になったものを取り上げて記事にしていきます。
+          取り上げて欲しい話題がありましたら、Twitterにご連絡ください！
+        </p>
+      </div>
       <p class="text-xl lg:text-xl font-semibold  border-b  p-3 pb-1">記事一覧</p>
       <g-link v-for="post in $page.posts.edges" :key="post.id" :to="post.node.path">
         <div class="mt-2 mx-5 border-b p-2 hover:bg-indigo-100">
